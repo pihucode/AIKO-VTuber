fbc08f5bc9f617bb395a194d8c1ea3efaa927789
April Huang <pinxhuang@gmail.com>
2023-03-23 17:39:01 -0400

    Create deepgram_test.py

---
+++ deepgram_test.py
@@ -0,0 +1,26 @@
+from deepgram import Deepgram
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEEPGRAM_API_KEY = os.getenv('DEEPGRAM_API_KEY')
+PATH_TO_FILE = 'eng_speech.wav'
+
+
+def speech_to_text():
+    deepgram = Deepgram(DEEPGRAM_API_KEY)
+    # Open the audio file
+    with open(PATH_TO_FILE, 'rb') as audio:
+        # ...or replace mimetype as appropriate
+        source = {'buffer': audio, 'mimetype': 'audio/wav'}
+        response = deepgram.transcription.sync_prerecorded(
+            source, {'punctuate': True})
+        # get transcript
+        transcript = response['results']['channels'][0]['alternatives'][0]['transcript']
+        print(transcript)
+        return transcript
+
+
+speech_to_text()
