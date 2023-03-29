33224bde5d5b84c1bdf3d9179b81a1356fde3613
April Huang <pinxhuang@gmail.com>
2023-03-29 15:54:52 -0400

    Render subtitles with tkinter

--- main.py
+++ main.py
@@ -4,32 +4,41 @@ from deepl_tutorial import translate
 from openai_utils import generate_response
 from voicevox_tut import text_to_speech
 import asyncio
-from playsound import playsound
 import sounddevice as sd
 import soundfile as sf
 from threading import Thread
+from subtitles_app import SubtitleApp
 
 VOICEVOX_FILENAME = 'voicevox_output.wav'
 
+subtitle_app = SubtitleApp()
+subtitle_app.start()
+
 
 async def main():
-    record_mic_audio()
-    prompt = speech_to_text()
+    subtitle_app.set_text('Hold down R to record audio')
+    # record_mic_audio()
+    # prompt = speech_to_text()
+    prompt = 'What do you like doing?'
+    print('Prompt: ' + prompt + '\n')
+
     response_text = generate_response(prompt)
-    print(response_text)
+
     translated_txt = translate(response_text, target_lang='JA')
-    print(translated_txt)
+    subtitle_app.set_text('Generating voice...')
+
     await text_to_speech(translated_txt)
+    subtitle_app.set_text(response_text)
 
     Thread(target=play_vtube_speakers).start()
     Thread(target=play_main_speakers).start()
 
-    await main()
+    # await main()
 
 
 def play_vtube_speakers():
     data1, fs1 = sf.read(VOICEVOX_FILENAME, dtype='float32')
-    sd.play(data1, fs1, device=5)
+    sd.play(data1, fs1, device=6)
     sd.wait()
 
 
