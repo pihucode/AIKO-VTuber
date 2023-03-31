4a192a60bf4f3ec1f32f572498f5106796d4702f
April Huang <pinxhuang@gmail.com>
2023-03-31 11:57:43 -0400

    Store chat history in chatbot

--- openai_utils.py
+++ openai_utils.py
@@ -11,24 +11,37 @@ def read_file(filepath):
         return infile.read()
 
 
-def generate_response(prompt):
-    print('Generating response...')
-    chat_history = [
-        {"role": "system", "content": read_file('prompt_chat.txt')},
-        {"role": "user", "content": prompt}
-    ]
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=chat_history,
-        temperature=0.8,
-        max_tokens=256,
-        frequency_penalty=0.5,
-        presence_penalty=.2,
-        stop=None,
-    )
-    text = response['choices'][0]['message']['content']
-    print(text)
-    return text
+class Chatbot:
+    def __init__(self):
+        self.message_history = [
+            {"role": "system", "content": read_file('prompt_chat.txt')},
+        ]
+
+    def generate_response(self, prompt):
+        print('Generating response...')
+
+        message = {"role": "user", "content": prompt}
+        self.message_history.append(message)
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=self.message_history,
+            temperature=0.65,
+            max_tokens=256,
+            frequency_penalty=0.9,
+            presence_penalty=0.4,
+            stop=None,
+        )
+        response_text = response['choices'][0]['message']['content']
+        print(response_text + '\n')
+
+        response_message = {
+            "role": "assistant",
+            "content": response_text,
+        }
+        self.message_history.append(response_message)
+
+        return response_text
 
 
 # prompt = 'What do you like to do?'
